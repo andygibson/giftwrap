1b0bec9620c9ca4e36b2fb44d79593aa69320454
Andy Gibson <contact@andygibson.net>
2010-09-17 21:53:33 +0000

    Updated the header stuff

--- giftwrap/trunk/src/main/java/org/fluttercode/giftwrap/elements/ManifestFile.java
+++ giftwrap/trunk/src/main/java/org/fluttercode/giftwrap/elements/ManifestFile.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright 2010, Andrew M Gibson
+ *
+ * www.andygibson.net
+ *
+ * This file is part of Giftwrap.
+ *
+ * Giftwrap is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Giftwrap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Giftwrap.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.fluttercode.giftwrap.elements;
 
 import java.io.File;
@@ -5,6 +28,10 @@ import java.io.File;
 import org.fluttercode.giftwrap.api.DeploymentContext;
 import org.fluttercode.giftwrap.impl.NamedResourceElement;
 
+/**
+ * @author Andy Gibson
+ * 
+ */
 public class ManifestFile extends NamedResourceElement {
 
 	private String fileName;
--- giftwrap/trunk/src/main/java/org/fluttercode/giftwrap/elements/ManifestResource.java
+++ giftwrap/trunk/src/main/java/org/fluttercode/giftwrap/elements/ManifestResource.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright 2010, Andrew M Gibson
+ *
+ * www.andygibson.net
+ *
+ * This file is part of Giftwrap.
+ *
+ * Giftwrap is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Giftwrap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Giftwrap.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.fluttercode.giftwrap.elements;
 
 import org.fluttercode.giftwrap.api.ContentProducer;
@@ -7,6 +30,10 @@ import org.fluttercode.giftwrap.impl.producer.StringContentProducer;
 import org.fluttercode.giftwrap.impl.producer.XmlProducer;
 import org.fluttercode.giftwrap.xml.DocumentNode;
 
+/**
+ * @author Andy Gibson
+ * 
+ */
 public class ManifestResource extends NamedResourceElement {
 
 	private ContentProducer producer;
