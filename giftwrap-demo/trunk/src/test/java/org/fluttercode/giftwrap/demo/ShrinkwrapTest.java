8130dbc35d82ed74e0ed262cbb19ac9eb5f616ff
Andy Gibson <contact@andygibson.net>
2010-09-17 21:48:55 +0000

    Major initial changes

--- giftwrap-demo/trunk/src/test/java/org/fluttercode/giftwrap/demo/ShrinkwrapTest.java
+++ giftwrap-demo/trunk/src/test/java/org/fluttercode/giftwrap/demo/ShrinkwrapTest.java
@@ -23,61 +23,69 @@
 
 package org.fluttercode.giftwrap.demo;
 
+import static org.fluttercode.giftwrap.xml.DocumentFactory.node;
 import static org.junit.Assert.assertEquals;
 
 import javax.inject.Inject;
 
+import org.fluttercode.giftwrap.api.ArchiveElement;
+import org.fluttercode.giftwrap.api.DeploymentContext;
 import org.fluttercode.giftwrap.demo.bean.DefaultRatingProducer;
 import org.fluttercode.giftwrap.demo.bean.NorthEastRatingProducer;
 import org.fluttercode.giftwrap.demo.bean.PersonManager;
+import org.fluttercode.giftwrap.demo.model.Person;
+import org.fluttercode.giftwrap.demo.model.RatingProducer;
+import org.fluttercode.giftwrap.elements.ManifestResource;
 import org.fluttercode.giftwrap.elements.jee.BeansXml;
 import org.fluttercode.giftwrap.impl.ArchiveRoot;
 import org.fluttercode.giftwrap.shrinkwrap.ArchiveDeployment;
+import org.fluttercode.giftwrap.xml.Document;
+import org.fluttercode.giftwrap.xml.DocumentNode;
+import org.fluttercode.giftwrap.xml.exporter.DocumentExporter;
 import org.jboss.arquillian.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ArchivePaths;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.ByteArrayAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-@RunWith(Arquillian.class)
 /**
+ * 
+ * Simple test class the doesn't have any profiles
+ * 
  * @author Andy Gibson
  * 
  */
-public class TestPersonManager {
+@RunWith(Arquillian.class)
+public class ShrinkwrapTest {
+
+	private final static String BEANS_FILE = "beans.xml";
 
 	@Inject
-	private PersonManager manager;
+	private PersonManager personManager;
 
 	@Deployment
-	public static Archive<?> createDeploymentPackage() {
-
-		ArchiveRoot root = new ArchiveRoot();
-		// this adds the person manager and everything it needs
-		root.addClass(PersonManager.class, true);
-
-		// two implementations of the same interface (not referenced in code,
-		// injected only so we can't automatically pick it up
-		root.addClass(DefaultRatingProducer.class);
-		root.addClass(NorthEastRatingProducer.class);
-
-		// define beans.xml
-		root.addElement(new BeansXml().add(
-				new BeansXml().addAlternative(DefaultRatingProducer.class)
-						.excludeFrom("NE")).add(
-				new BeansXml().addAlternative(NorthEastRatingProducer.class).includeIn("NE")));
-
-		//Create the archive for the North Easy profile		
-		JavaArchive ar = ArchiveDeployment.generate(root, "NE");
-		return ar;
-
+	public static Archive<?> createDeploy() {
+		String beans = "<beans><alternatives><class>"+DefaultRatingProducer.class.getName()+"</class></alternatives></beans>";
+		return ShrinkWrap.create(JavaArchive.class, "test.jar")
+				.addManifestResource(new ByteArrayAsset(beans.getBytes()),ArchivePaths.create("beans.xml"))
+				.addPackages(true,DefaultRatingProducer.class.getPackage())
+				.addPackages(true,Person.class.getPackage());
+				
+				
 	}
+
 
 	@Test
 	public void testManager() {
-		manager.applyRating();
-		assertEquals(1, manager.getPerson().getRating());
+		Assert.assertNotNull(personManager);
+		personManager.applyRating();
+		assertEquals(28, personManager.getPerson().getRating());
 	}
+
 
 }
--- giftwrap-demo/trunk/src/test/java/org/fluttercode/giftwrap/demo/SimpleTest.java
+++ giftwrap-demo/trunk/src/test/java/org/fluttercode/giftwrap/demo/SimpleTest.java
@@ -23,38 +23,44 @@
 
 package org.fluttercode.giftwrap.demo;
 
+import static org.fluttercode.giftwrap.xml.DocumentFactory.node;
 import static org.junit.Assert.assertEquals;
-import static org.fluttercode.giftwrap.xml.DocumentFactory.*;
-
-import java.util.Map;
 
 import javax.inject.Inject;
 
 import org.fluttercode.giftwrap.api.ArchiveElement;
+import org.fluttercode.giftwrap.api.DeploymentContext;
 import org.fluttercode.giftwrap.demo.bean.DefaultRatingProducer;
+import org.fluttercode.giftwrap.demo.bean.NorthEastRatingProducer;
 import org.fluttercode.giftwrap.demo.bean.PersonManager;
+import org.fluttercode.giftwrap.demo.model.Person;
+import org.fluttercode.giftwrap.demo.model.RatingProducer;
 import org.fluttercode.giftwrap.elements.ManifestResource;
+import org.fluttercode.giftwrap.elements.jee.BeansXml;
 import org.fluttercode.giftwrap.impl.ArchiveRoot;
 import org.fluttercode.giftwrap.shrinkwrap.ArchiveDeployment;
 import org.fluttercode.giftwrap.xml.Document;
 import org.fluttercode.giftwrap.xml.DocumentNode;
-import org.fluttercode.giftwrap.xml.DocumentNodeAttribute;
+import org.fluttercode.giftwrap.xml.exporter.DocumentExporter;
 import org.jboss.arquillian.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ArchivePath;
-import org.jboss.shrinkwrap.api.Node;
+import org.jboss.shrinkwrap.api.ArchivePaths;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.ByteArrayAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-@RunWith(Arquillian.class)
 /**
+ * 
+ * Simple test class the doesn't have any profiles
+ * 
  * @author Andy Gibson
  * 
  */
+@RunWith(Arquillian.class)
 public class SimpleTest {
 
 	private final static String BEANS_FILE = "beans.xml";
@@ -64,29 +70,48 @@ public class SimpleTest {
 
 	@Deployment
 	public static Archive<?> createDeploy() {
-		return ShrinkWrap.create(JavaArchive.class, "test.jar").addPackage(
-				ArchiveElement.class.getPackage())
-				.addClass(PersonManager.class);
+		String beans = "<beans><alternatives><class>"+DefaultRatingProducer.class.getName()+"</class></alternatives></beans>";
+		return ShrinkWrap.create(JavaArchive.class, "test.jar")
+				.addManifestResource(new ByteArrayAsset(beans.getBytes()),ArchivePaths.create("beans.xml"))
+				.addPackages(true,DefaultRatingProducer.class.getPackage())
+				.addPackages(true,Person.class.getPackage());
+				
+				
 	}
 
-	@Deployment
 	public static Archive<?> createDeploymentPackage() {
 
 		ArchiveRoot root = new ArchiveRoot();
-		root.addClass(PersonManager.class, false);
-		root.addClass(DefaultRatingProducer.class);
-		root.addPackage(DocumentNode.class.getPackage());
-		root.addClass(DocumentNode.class);
-		root.addClass(Document.class);
-		root.addClass(DocumentNodeAttribute.class);
-		
-		// root.addClass(ArchiveElement.class);
-		// root.addClass(DeploymentContext.class);
-		// root.addClass(ElementContainer.class,true);
-		// root.addClass(NorthEastRatingProducer.class);
-		// root.addClass(DefaultRatingProducer.class);
-		// root.addElement(new BeansXmlElement());
 
+		// this adds the person manager and everything it needs
+		root.addClass(PersonManager.class, true);
+
+		// two implementations of the same interface (not referenced in code,
+		// injected only so we can't automatically pick it up
+		root.addClass(DefaultRatingProducer.class);
+
+		// define beans.xml
+		new BeansXml().addAlternative(DefaultRatingProducer.class).addTo(root);
+
+		// create the list of archive
+		JavaArchive ar = ArchiveDeployment.generate(root);
+		return ar;
+
+	}
+
+	// @Deployment
+	public static Archive<?> workingDeploymentPackage() {
+
+		ArchiveRoot root = new ArchiveRoot();
+		root.addClass(PersonManager.class, true);
+		root.addClass(DefaultRatingProducer.class);
+		root.addClass(RatingProducer.class);
+		root.addClass(ArchiveElement.class);
+		root.addClass(NorthEastRatingProducer.class);
+		root.addPackage(DocumentNode.class.getPackage());
+
+		root.addClass(DeploymentContext.class);
+		root.addClass(DocumentExporter.class);
 		Document beans = new Document("beans.xml");
 		beans
 				.add(node("alternatives")
@@ -96,31 +121,7 @@ public class SimpleTest {
 												"org.fluttercode.giftwrap.demo.bean.DefaultRatingProducer")));
 
 		root.addElement(new ManifestResource("beans.xml", beans));
-
-		/*
-		 * root.addElement(new PartialManifestFileElement(BEANS_FILE,
-		 * "<beans>",-99)) .addElement(new
-		 * PartialManifestFileElement(BEANS_FILE, "<alternatives>",-10))
-		 * .addElement(new PartialManifestFileElement(BEANS_FILE,
-		 * "<class>org.fluttercode.giftwrap.demo.bean.NorthEastRatingProducer</class>"
-		 * ,0).addIncludeProfile("NE")) .addElement(new
-		 * PartialManifestFileElement(BEANS_FILE,
-		 * "<class>org.fluttercode.giftwrap.demo.bean.DefaultRatingProducer</class>"
-		 * ,0).addExcludeProfile("NE")) .addElement(new
-		 * PartialManifestFileElement(BEANS_FILE, "</alternatives>",10))
-		 * .addElement(new PartialManifestFileElement(BEANS_FILE,
-		 * "</beans>",100));
-		 */
-
-		// ArchiveDeployment dp = new ArchiveDeployment();
-		// dp.addProfile("NE");
-
 		JavaArchive ar = ArchiveDeployment.generate(root);
-		System.out.println("Archive  = " + ar);
-		Map<ArchivePath, Node> map = ar.getContent();
-		for (ArchivePath path : map.keySet()) {
-			System.out.println(path + " - " + map.get(path));
-		}
 		return ar;
 
 	}
@@ -130,6 +131,18 @@ public class SimpleTest {
 		Assert.assertNotNull(personManager);
 		personManager.applyRating();
 		assertEquals(28, personManager.getPerson().getRating());
+	}
+
+	public static Document getBeansDoc() {
+		Document beans = new Document("beans.xml");
+		beans
+				.add(node("alternatives")
+						.add(
+								node("class")
+										.value(
+												"org.fluttercode.giftwrap.demo.bean.DefaultRatingProducer")));
+
+		return beans;
 	}
 
 }
